568418f8542a3e4ba7e4856647ff341299e47ff9
Angellopp <98242498+Angellopp@users.noreply.github.com>
2023-09-04 13:03:52 -0500

    Add files via upload

---
+++ PE.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector <int> A, B;
+    int a, b, aux;
+    int ans1 = 0, ans2 = 0;
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        cin >> aux;
+        if (aux <= 1440) ans1++;
+        A.push_back(aux);
+    }
+    
+    cin >> b;
+    for (int i = 0; i < b; i++) {
+        cin >> aux;
+        if (aux <= 1440) ans1++;
+        B.push_back(aux);
+    }
+    
+    bool state = true;
+    int pa = 0, pb = 0;
+    int i = 0, j = 0;
+    while (i != a and j != b){
+            if (B[j] > A[i]) { 
+                pa++;
+                i++;
+            } else {
+                pb++;
+                j++;
+            }
+            if (pb > pa) {
+                if (state) {
+                    ans2++;
+                    cout << "yala\n";
+                }
+                state = false;
+            }
+            else if (pa > pb) {
+                state = true;
+            }
+        
+    }
+    cout << ans1 << "\n" << ans2 << endl <<pa<<endl<<pb;
+    
+}
